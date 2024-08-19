425c4eb9246fa874ad500a1ead5813b0e748f068
Vlatko <vlatkobogojevski@yahoo.com>
2024-08-19 13:28:23 +0200

    update angular cli

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ angular-course/src/app/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    standalone: true,
+    styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {}
