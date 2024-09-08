4bfd087c54e06e76f6541a45a6373778ecf9cd81
Vlatko <vlatkobogojevski@yahoo.com>
2024-09-09 01:35:50 +0200

    Introduction to signals

--- angular-course/src/app/user/user.component.ts
+++ angular-course/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,6 +12,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[randomIndex]
+  // selectedUser = DUMMY_USERS[randomIndex] before signals
+  selectedUser = signal(DUMMY_USERS[randomIndex]); // with signals
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar) // with signals we use computed()
+
+  // get imagePath () {
+  //   return 'assets/users/' + this.selectedUser.avatar  //without signals, we can use getter
+  // }
+
+  onSelectUser(): void {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]) //with signals
+    // this.selectedUser = DUMMY_USERS[randomIndex] //without signals
+  }
 
 }
